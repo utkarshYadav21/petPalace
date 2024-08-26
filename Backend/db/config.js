d128bd859ceaafd3c5080dbd27c2f4a40b08b89b
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-08-26 18:23:58 +0530

    minor fix

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const dbURL =
-  "mongodb+srv://utkarshyadav329:22utkarsh22@cluster0.w14eg7b.mongodb.net/Adoption?retryWrites=true&w=majority&appName=Cluster0";
+  "PUT URL TO YOUR OWN DATABASE";
 mongoose
   .connect(dbURL, {
     useNewUrlParser: true,
